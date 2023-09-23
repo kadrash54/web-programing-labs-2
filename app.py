d31b6aa9b4e7ab33d9140caccf572aef822ac07d
Кадралеев Кирилл <Kadraleev.kirill@gmail.com>
2023-09-23 15:43:46 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    </header>
+    <h1>web-сервер на flusk</h1>
+    <footer>
+    </footer>
+</body>
+</html>
+"""
