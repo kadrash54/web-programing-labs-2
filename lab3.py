3125bf5c098553834497fe82734943290caf8caf
Кадралеев Кирилл <kadraleev.kirill@gmail.com>
2023-11-18 00:51:50 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,7 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 lab2 = Blueprint('lab3', __name__)
-
 
 @lab3.route('/lab3/')
 def lab():
     return render_template('lab3.html')
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    errors = {}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    sex = request.args.get('sex')
+    return render_template('form1.html',
+                           user=user,
+                           age=age,
+                           sex=sex,
+                           errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+
+    if drink == 'coffe':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('pay.html', price=price)
