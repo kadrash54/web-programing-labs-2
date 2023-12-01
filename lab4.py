ae59650b211fd49aa513cc7a66969b3925ea3ab5
Кадралеев Кирилл <kadraleev.kirill@gmail.com>
2023-12-02 01:54:29 +0700

    Заготоука labi

--- lab4.py
+++ lab4.py
@@ -5,3 +5,31 @@ lab4 = Blueprint('lab4', __name__)
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    errors = {}
+
+    if request.method == 'GET':
+        return render_template("login.html", errors=errors)
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == '':
+        errors['username'] = 'не введен логин'
+
+    if password == '':
+        errors['password'] = 'не введен пароль'
+
+    if username == 'alex' and password == '123':
+        return render_template('success_login.html', username=username)
+
+    elif username != '' and password != '':
+        errors['h'] = 'Неверный логин и/или пароль'
+
+    return render_template('login.html', username=username,
+                           password=password, errors=errors)
+
+                           
