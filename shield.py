22218a5fbdd2d85d6a3a27ec509dd6c05e8a8e5d
Кадралеев Кирилл <kadraleev.kirill@gmail.com>
2023-12-16 14:41:13 +0700

    clean garbage

--- shield.py
+++ shield.py
@@ -3,22 +3,22 @@ from flask import Blueprint, render_template, request
 lab3 = Blueprint('lab3', __name__)
 
 
-@lab3.route('/lab3/armor')
-def armor1():
-x = request.args.get('x')
-n = request.args.get('n')
-result = 0
-if x != None and n != None:
-x = int(x)
-i = int(n)
-for i in range(i, 0, -1):
-result += (-1) ** i * x ** (2 * i + 1) / math.factorial((2 * i + 1))
-return render_template('shield2.html', x=x, n=n, result=result)
+@lab3.route('/lab3/shield')
+def shield1():
+    x = request.args.get('x')
+    n = request.args.get('n')
+    result = 0
+    if x != None and n != None:
+    x = int(x)
+    i = int(n)
+    for i in range(i, 0, -1):
+    result += (-1) ** i * x ** (2 * i + 1) / math.factorial((2 * i + 1))
+    return render_template('shield2.html', x=x, n=n, result=result)
 
 
 #if 19
-@lab3.route('/lab3/armor2')
-def armor2():
+@lab3.route('/lab3/shield2')
+def shield2():
 errors = {}
 n1 = request.args.get('n1')
 n2 = request.args.get('n2')
